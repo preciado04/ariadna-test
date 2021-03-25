6ad98cab11bade014491a815a11106d9789674b5
Abraham Preciado Fonseca <preciado0425@gmail.com>
2021-03-24 18:03:27 -0700

    Created Ariadna custom module.

---
+++ web/modules/custom/ariadna/src/Controller/SpotifyController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\ariadna\Controller;
+
+class SpotifyController {
+  public function spotify() {
+    return array(
+      '#title' => 'Ariadna Module',
+      '#markup' => 'This module is for demo purposes',
+    );
+  }
+}
